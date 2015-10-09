0900be1bc52d9e2b922705f69e295a80f35868f5
Ryan Walden <Ryan Walden@purdue.edu>
2015-10-09 13:03:17 -0400

    fixed indent error

--- calc.py
+++ calc.py
@@ -78,7 +78,7 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
-		elif (op == "**"):
+        elif (op == "**"):
 			print "Result: ", exp(a, b)
 		elif (op == "%"):
 			print "Result: ", mod(a, b)
